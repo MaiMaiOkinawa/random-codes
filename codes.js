5333494855c0785bf80c765275a50493ad60f655
Maï Uchida <maiuchidakishaba@gmail.com>
2022-12-21 19:18:22 +0100

    Adding data

--- codes.js
+++ codes.js
@@ -55,4 +55,76 @@ const localQuotes = [
       text: "The values() method returns a new array iterator object that iterates the value of each index in the array.",
       author: 'Array.prototype.values()',
     },
+    {
+      text: "The length data property of an Array instance represents the number of elements in that array. The value is an unsigned, 32-bit integer that is always numerically greater than the highest index in the array.",
+      author: 'Array.prototype.length',
+    },
+    {
+      text: "The @@iterator method of an Array object implements the iterable protocol and allows arrays to be consumed by most syntaxes expecting iterables, such as the spread syntax and for...of loops. It returns an iterator that yields the value of each index in the array. The initial value of this property is the same function object as the initial value of the Array.prototype.values property.",
+      author: 'Array.prototype[@@iterator]()',
+    },
+    {
+      text: "The at() method takes an integer value and returns the item at that index, allowing for positive and negative integers. Negative integers count back from the last item in the array.",
+      author: 'Array.prototype.at()',
+    },
+    {
+      text: "The concat() method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array.",
+      author: 'Array.prototype.concat()',
+    },
+    {
+      text: "The copyWithin() method shallow copies part of an array to another location in the same array and returns it without modifying its length.",
+      author: 'Array.prototype.copyWithin()',
+    },
+    {
+      text: "The entries() method returns a new Array Iterator object that contains the key/value pairs for each index in the array.",
+      author: 'Array.prototype.entries()',
+    },
+    {
+      text: "The every() method tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value.",
+      author: 'Array.prototype.every()',
+    },
+    {
+      text: "The fill() method changes all elements in an array to a static value, from a start index (default 0) to an end index (default array.length). It returns the modified array.",
+      author: 'Array.prototype.fill()',
+    },
+    {
+      text: "The filter() method creates a shallow copy of a portion of a given array, filtered down to just the elements from the given array that pass the test implemented by the provided function.",
+      author: 'Array.prototype.filter()',
+    },
+    {
+      text: "The find() method returns the first element in the provided array that satisfies the provided testing function. If no values satisfy the testing function, undefined is returned.",
+      author: 'Array.prototype.find()',
+    },
+    {
+      text: "The findIndex() method returns the index of the first element in an array that satisfies the provided testing function. If no elements satisfy the testing function, -1 is returned.",
+      author: 'Array.prototype.findIndex()',
+    },
+    {
+      text: "The findLast() method iterates the array in reverse order and returns the value of the first element that satisfies the provided testing function. If no elements satisfy the testing function, undefined is returned.",
+      author: 'Array.prototype.findLast()',
+    },
+    {
+      text: "The findLastIndex() method iterates the array in reverse order and returns the index of the first element that satisfies the provided testing function. If no elements satisfy the testing function, -1 is returned.",
+      author: 'Array.prototype.findLastIndex()',
+    },
+    {
+      text: "The flat() method creates a new array with all sub-array elements concatenated into it recursively up to the specified depth.",
+      author: 'Array.prototype.flat()',
+    },
+    {
+      text: "The flatMap() method returns a new array formed by applying a given callback function to each element of the array, and then flattening the result by one level. It is identical to a map() followed by a flat() of depth 1 (arr.map(...args).flat()), but slightly more efficient than calling those two methods separately.",
+      author: 'Array.prototype.flatMap()',
+    },
+    {
+      text: "The forEach() method executes a provided function once for each array element.",
+      author: 'Array.prototype.forEach()',
+    },
+    {
+      text: "The Array.from() static method creates a new, shallow-copied Array instance from an iterable or array-like object.",
+      author: 'Array.from()',
+    },
+    // {
+    //   text: "",
+    //   author: '',
+    // } 
 ]
